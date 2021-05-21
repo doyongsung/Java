56b3fe82aa6c0b04b461be7122cff17912ab6864
DESKTOP-T3DOGRQ\user <ppelpp@naver.com>
2021-05-21 12:54:44 +0900

    5-21

--- javaworkshop/src/main/java/javaworkshop/Sample01.java
+++ javaworkshop/src/main/java/javaworkshop/Sample01.java
@@ -3,9 +3,9 @@ package javaworkshop;
 public class Sample01 {
 
 	public static void main(String[] args) {
-		System.out.println("Test Java");
+		System.out.println("Test Javaasdsadsadsa");
 	
-
+a
 	}
 
 }
